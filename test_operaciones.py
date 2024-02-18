f31c2dde4210c6ffd43a86a75eb0585a277d5822
Jorge975 <119123021+Jorge975@users.noreply.github.com>
2024-02-18 17:42:07 +0100

    Update test_operaciones.py
    Arreglo el test, para mostrar más tipos de pruebas

--- test_operaciones.py
+++ test_operaciones.py
@@ -6,9 +6,20 @@ class TestSumar(unittest.TestCase):
  
  def test_sumar(self):
     self.assertEqual(sumar(3, 2), 5)
+    self.assertEqual(sumar(-3,3), 0)
+    self.assertEqual(sumar(-3,-2), -5)
+
     self.assertEqual(restar(8, 3), 5)
+    self.assertEqual(restar(3, 3), 0)
+    self.assertEqual(restar(-8, 2), -10)
+
     self.assertEqual(multiplicar(5, 1), 5)
+    self.assertEqual(multiplicar(5, 0), 0)
+    self.assertEqual(multiplicar(-5, 1), -5)
+
     self.assertEqual(dividir(10,2), 5)
+    self.assertEqual(dividir(5,2), 2)
+    self.assertEqual(dividir(-10,2), -5)
 
 if __name__ == '__main__':
  unittest.main()
